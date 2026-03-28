cf4f8a3d999b780b18024a6c0acbb619bb14b707
anikh174 <hossainanik174@gmail.com>
2026-03-28 22:54:26 +0600

    added tabs option -Models -Carts

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import Banner from "./components/Banner";
+import Cart from "./components/Cart";
 import Footer from "./components/Footer";
 import Models from "./components/Models";
 import NavBar from "./components/Navbar";
 
-const getModels = async()=>{
+const getModels = async () => {
   const res = fetch("/models.json");
   return (await res).json();
-}
+};
 
 const modelPromise = getModels();
 
@@ -16,7 +17,25 @@ function App() {
     <>
       <NavBar></NavBar>
       <Banner></Banner>
+
+      <div className="tabs tabs-box justify-center bg-transparent">
+        <input
+          type="radio"
+          name="my_tabs_1"
+          className="tab rounded-full w-40"
+          aria-label="Models"
+          defaultChecked
+        />
+        <input
+          type="radio"
+          name="my_tabs_1"
+          className="tab rounded-full w-40"
+          aria-label="Cart"
+        />
+      </div>
+
       <Models modelPromise={modelPromise}></Models>
+      <Cart></Cart>
       <Footer></Footer>
     </>
   );
---
+++ src/components/Cart.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cart = () => {
+    return (
+        <div className='bg-red-500 p-4'>
+            Cart:
+        </div>
+    );
+};
+
+export default Cart;
